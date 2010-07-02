7f206060fa752e8a8a7a5daadeac095aad404f9a
lucazeau.alexis <lucazeau.alexis@32e6bcfa-9e84-7d95-9935-f22b4b10203c>
2010-07-02 12:38:53 +0000

    petite correction orthographique

--- trunk/include/shared_library/SharedLibrary_unix.h
+++ trunk/include/shared_library/SharedLibrary_unix.h
@@ -45,7 +45,7 @@ class		SharedLibrary_unix : public ISharedLibrary
 		/*!
 		 * \brief	Close and unmap the library from the memory
 		 * \param	The handler of the library
-		 * \return	true if success, false else
+		 * \return	true if success, false otherwise
 		 */
 		bool			libFree(libHandler lib);
 };
