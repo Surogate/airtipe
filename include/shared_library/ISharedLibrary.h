c8b6b1fa0ae6e46540baf95c90d5f9642c9436f1
lucazeau.alexis <lucazeau.alexis@32e6bcfa-9e84-7d95-9935-f22b4b10203c>
2010-07-02 12:28:53 +0000

    Commentaires sur les Shared library

--- include/shared_library/ISharedLibrary.h
+++ include/shared_library/ISharedLibrary.h
@@ -1,3 +1,11 @@
+/*!
+ * \file	ISharedLibrary.h
+ * \brief	Interface for system abstraction of shared library
+ * \author	Lucazeau Alexis - lucaze_b@epitech.eu
+ * \version	0.1
+ * \date	02/07/2010 11:25:07
+ */
+
 #ifndef		ISHAREDLIBRARY_H__
 # define	ISHAREDLIBRARY_H__
 
@@ -12,11 +20,33 @@
 	typedef	FARPROC libProcAddr;
 # endif
 
+/*!
+ * \class	ISharedLibrary
+ * \brief	Interface for abstraction of shared library
+ */
 class		ISharedLibrary
 {
 	public:
+		/*!
+		 * \brief	Load library
+		 * \param	Name of the library file to load
+		 * \return	The handler of loaded library
+		 */
 		virtual libHandler		libLoad(std::string const & filename) = 0;
+
+		/*!
+		 * \brief	Get pointer on function in a library
+		 * \param	The handler of the library to search in
+		 * \param	The name of the function to find out
+		 * \return	The pointer of the found function
+		 */
 		virtual libProcAddr		libGetProc(libHandler lib, std::string const & funcname) = 0;
+
+		/*!
+		 * \brief	Close and unmap the library from the memory
+		 * \param	The handler of the library
+		 * \return	true if success, false else
+		 */
 		virtual bool			libFree(libHandler lib) = 0;
 };
 
--- include/shared_library/SharedLibrary_unix.h
+++ include/shared_library/SharedLibrary_unix.h
@@ -1,16 +1,52 @@
+/*!
+ * \file	SharedLibrary_unix.h
+ * \brief	Implementation for system abstraction of shared library
+ * \author	Lucazeau Alexis - lucaze_b@epitech.eu
+ * \version	0.1
+ * \date	02/07/2010 11:25:07
+ */
+
 #ifndef		SHAREDLIBRARY_UNIX_H__
 # define	SHAREDLIBRARY_UNIX_H__
 
 # include	"ISharedLibrary.h"
 
+/*!
+ * \class	SharedLibrary_unix
+ * \brief	Implementation for abstraction of shared library
+ */
 class		SharedLibrary_unix : public ISharedLibrary
 {
 	public:
+		/*!
+		 * \brief	Default constructor for SharedLibrary_unix
+		 */
 		SharedLibrary_unix();
+		/*!
+		 * \brief	Destructo for SharedLibrary_unix
+		 */
 		virtual ~SharedLibrary_unix();
 
+		/*!
+		 * \brief	Load library
+		 * \param	Name of the library file to load
+		 * \return	The handler of loaded library
+		 */
 		libHandler		libLoad(std::string const & filename);
+
+		/*!
+		 * \brief	Get pointer on function in a library
+		 * \param	The handler of the library to search in
+		 * \param	The name of the function to find out
+		 * \return	The pointer of the found function
+		 */
 		libProcAddr		libGetProc(libHandler lib, std::string const & funcname);
+
+		/*!
+		 * \brief	Close and unmap the library from the memory
+		 * \param	The handler of the library
+		 * \return	true if success, false else
+		 */
 		bool			libFree(libHandler lib);
 };
 
