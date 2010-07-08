e0fc5296fa20a37c8028d2db6b31808e403adc87
lucazeau.alexis <lucazeau.alexis@32e6bcfa-9e84-7d95-9935-f22b4b10203c>
2010-07-08 15:18:13 +0000

    Integration PacketManager au server ok + map d'action en fonction d'unpacket code -> reste a  reecrire read pour envoyer sur la bonne action en fonction du header recupere

--- trunk/Server/src/Server.cpp
+++ trunk/Server/src/Server.cpp
@@ -12,7 +12,18 @@
 Server::Server(short port, unsigned int bufferSize) :
 	TCPServer(port, bufferSize)
 {
+	this->_actions.insert(std::pair<PacketCode, Action>(Login, &Server::ActionLogin));
+	this->_actions.insert(std::pair<PacketCode, Action>(CreateGame, &Server::ActionCreateGame));
 
+	this->_actions.insert(std::pair<PacketCode, Action>(AddMap, &Server::ActionAddMap));
+	this->_actions.insert(std::pair<PacketCode, Action>(ValidGame, &Server::ActionValidGame));
+	this->_actions.insert(std::pair<PacketCode, Action>(JoinGame, &Server::ActionJoinGame));
+	this->_actions.insert(std::pair<PacketCode, Action>(ChooseSpacecraft, &Server::ActionChooseSpacecraft));
+	this->_actions.insert(std::pair<PacketCode, Action>(Ready, &Server::ActionReady));
+	this->_actions.insert(std::pair<PacketCode, Action>(NotReady, &Server::ActionNotReady));
+	this->_actions.insert(std::pair<PacketCode, Action>(StartGame, &Server::ActionStartGame));
+	this->_actions.insert(std::pair<PacketCode, Action>(StopGame, &Server::ActionStopGame));
+	this->_actions.insert(std::pair<PacketCode, Action>(QuitGame, &Server::ActionQuitGame));
 }
 
 Server::~Server()
@@ -32,3 +43,82 @@ bool	Server::accept()
 	}
 	return (false);
 }
+
+Packet *	Server::ActionLogin(Packet * pak)
+{
+	pak = pak;
+	std:: cout << "Action: Login" << std::endl;
+	return NULL;
+}
+
+
+Packet *	Server::ActionCreateGame(Packet * pak)
+{
+	pak = pak;
+	std:: cout << "Action: CreateGame" << std::endl;
+	return NULL;
+}
+
+Packet *	Server::ActionAddMap(Packet * pak)
+{
+	pak = pak;
+	std:: cout << "Action: AddMap" << std::endl;
+	return NULL;
+}
+
+Packet *	Server::ActionValidGame(Packet * pak)
+{
+	pak = pak;
+	std:: cout << "Action: ValidGame" << std::endl;
+	return NULL;
+}
+
+Packet *	Server::ActionJoinGame(Packet * pak)
+{
+	pak = pak;
+	std:: cout << "Action: JoinGame" << std::endl;
+	return NULL;
+}
+
+Packet *	Server::ActionChooseSpacecraft(Packet * pak)
+{
+	pak = pak;
+	std:: cout << "Action: ChooseSpacecraft" << std::endl;
+	return NULL;
+}
+
+Packet *	Server::ActionReady(Packet * pak)
+{
+	pak = pak;
+	std:: cout << "Action: Ready" << std::endl;
+	return NULL;
+}
+
+Packet *	Server::ActionNotReady(Packet * pak)
+{
+	pak = pak;
+	std:: cout << "Action: NotReady" << std::endl;
+	return NULL;
+}
+
+Packet *	Server::ActionStartGame(Packet * pak)
+{
+	pak = pak;
+	std:: cout << "Action: StartGame" << std::endl;
+	return NULL;
+}
+
+Packet *	Server::ActionStopGame(Packet * pak)
+{
+	pak = pak;
+	std:: cout << "Action: StopGame" << std::endl;
+	return NULL;
+}
+
+Packet *	Server::ActionQuitGame(Packet * pak)
+{
+	pak = pak;
+	std:: cout << "Action: QuitGame" << std::endl;
+	return NULL;
+}
+
