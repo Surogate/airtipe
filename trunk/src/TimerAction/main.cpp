63f072dba6fe3bf40e96e8de6ba7404d0850dadb
FrancoisAncel <FrancoisAncel@32e6bcfa-9e84-7d95-9935-f22b4b10203c>
2010-07-09 10:16:52 +0000

    correct missed function

--- trunk/src/TimerAction/main.cpp
+++ trunk/src/TimerAction/main.cpp
@@ -7,21 +7,21 @@
 
 #include <iostream>
 
-#include "WTimerManager.hpp"
-#include <windows.h>
+#include "LTimerManager.hpp"
+//#include <windows.h>
 /*
  * 
  */
 int main(void) {
     int t = 0;
-    WTimerManager time;
+    LTimerManager time;
     ITimerManager::timeHdl titi = time.setTimer();
     mtime toto = time.GetActualTime();
     if (!toto.valid)
         std::cout << "error" << std::endl;
     std::cout << "second " << toto.sec << std::endl;
     std::cout << "usecond " << toto.msec << std::endl;
-    Sleep(2);
+    sleep(2);
     ITimerManager::timeHdl tata = time.setTimer();
     toto = time.GetUnTimeBetween(tata, titi);
     if (!toto.valid)
