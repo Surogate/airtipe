f920afe8f3eed2fc12b155ef818dc595c78e92ce
bertran.pierre <bertran.pierre@32e6bcfa-9e84-7d95-9935-f22b4b10203c>
2010-07-13 12:26:24 +0000

    Un debut d'abstraction network windows.

---
+++ trunk/Common/include/network/CTCPSession_Win32.h
@@ -0,0 +1,19 @@
+/*!
+ * \file	TCPData.h
+ * \brief	
+ * \author	Bertran Pierre - bertra_b@epitech.eu
+ * \version	0.1
+ * \date	07/07/2010 17:31:51
+ */
+
+#ifndef		CTCPSESSION_WIN32_H__
+# define	CTCPSESSION_WIN32_H__
+
+# include	"network/ITCPSession.h"
+
+class		CTCPSession_Win32 : public ITCPSession
+{
+	
+};
+
+#endif		// CTCPSESSION_WIN32_H__
---
+++ trunk/Common/src/network/CTCPServer_Win32.cpp
@@ -0,0 +1,9 @@
+/*!
+ * \file	TCPData.h
+ * \brief	
+ * \author	Bertran Pierre - bertra_b@epitech.eu
+ * \version	0.1
+ * \date	07/07/2010 17:31:51
+ */
+
+#include "network/CTCPServer_Win32.h"
